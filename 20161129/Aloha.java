3f3524d3c8682c07d22dcfce00849d43fdbd1625
iamnoone999 <160139@yses.ac.jp>
2016-11-29 09:47:41 +0900

    今日作ったフォルダ

--- 20161129/Aloha.java
+++ 20161129/Aloha.java
@@ -1,11 +1,5 @@
 public class Aloha{
 	public static void main(String[] args) {
-		for (int i=0 ; i<20 ; i++){
-			if (i%4==0){
-		System.out.println("アロハ");
-	}else{
-		System.out.println("Aloha");
-	}
-	}
+		System.out.println(args[0]+ "さん" +"おはようございます");
 	}
 }
