7c18611b8f76f4efbc4ebc61e46d3577101137a1
iamnoone999 <160139@yses.ac.jp>
2016-12-13 09:38:53 +0900

    Scanner,exitを入力したら終わりになります

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -1,15 +1,20 @@
-public class Aloha{
+public class Aloha {
 	public static void main (String[] args) {
 	
 	String name;
 
-	
+	while (true){
 	System.out.println("名前を入力してください >");
 
 
 	name = new java.util.Scanner(System.in).nextLine();
-
-	
 	System.out.println(name);
+	
+if (name.equals("exit") ){
+	   break;
+
+}
+
 	}
 }
+	}
