4e0d4e6703918aa88ba65bcc8139cdd76b98b6e0
iamnoone999 <160139@yses.ac.jp>
2016-12-13 09:25:32 +0900

    ２０回を４回作りました

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -1,10 +1,15 @@
 public class Aloha{
 	public static void main(String[] args) {
-		int i=0;
-		while ( i<20){
-
+	for (int i=0; i<20; i++){
+		if(i%4==0){
+			System.out.println("アロハ");
+		}else{
 			System.out.println("ALOHA!");
-			i++;
+
 		}
+	}
+
+			
+		
 	}
 }
